12ab479c7ba57c6b04a5f2531327b7e524c815a1
luqmanbrainz97 <hakimbrainz97@gmail.com>
2021-01-14 17:55:27 +0800

    Select-Campus template

---
+++ src/app/select-campus/select-campus.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-campus',
+  templateUrl: './select-campus.page.html',
+  styleUrls: ['./select-campus.page.scss'],
+})
+export class SelectCampusPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
